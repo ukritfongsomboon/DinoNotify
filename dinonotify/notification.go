99a20954774f052733172ab82313200770e7b19f
Ukrit Fongsomboon <ukrit.fb@gmail.com>
2026-04-23 17:26:52 +0700

    rename provider files to notification and merge payloay_format into notification

--- dinonotify/notification.go
+++ dinonotify/notification.go
@@ -1,5 +1,10 @@
 package dinonotify
 
+import (
+	"fmt"
+	"time"
+)
+
 // Model ===========================
 type Severity string
 
@@ -56,4 +61,13 @@ type ProviderMessage interface {
 	Success(message MessagePayload) error
 	Warning(message MessagePayload) error
 	SendFile(file FilePayload) error
+}
+
+// Format ==========================
+const separator = "──────────────"
+
+func FormatMessage(severity Severity, p MessagePayload) string {
+	e := severity.emoji()
+	header := fmt.Sprintf("%s %s (%s)", e, severity, time.Now().Format("2006-01-02 15:04"))
+	return fmt.Sprintf("%s\n%s\n%s\n%s\n%s\n%s", header, separator, p.Title, p.Subtitle, separator, p.Message)
 }
